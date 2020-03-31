f1eb9731f076df1d2bb960241c839c3e885d2516
tk <ec2-user@ip-172-31-45-101.us-east-2.compute.internal>
2020-03-31 16:24:15 +0000

    first

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ApplicationRecord
+  
+  #VALID_PASS_REGEX = /\A(?=.*?[a-z])[a-z\d]+\z/
+  VALID_PASS_REGEX = /\A(?=.?[a-z])(?=.*?\d)[a-z\d]{8,32}/
+  VALID_MAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/
+  
+  validates :name, presence: true, length:{maximum:15}
+  validates :email, presence: true, format: {with: VALID_MAIL_REGEX}
+  validates :password, presence: true, format: {with: VALID_PASS_REGEX}
+  
+  has_secure_password
+  
+  has_many :topics
+end
