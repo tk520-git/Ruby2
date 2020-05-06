491a3676f92271217017cb275dc4e3b04c13130b
tk <ec2-user@ip-172-31-45-101.us-east-2.compute.internal>
2020-05-06 11:26:48 +0000

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_173809) do
+ActiveRecord::Schema.define(version: 2020_04_24_182817) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "topic_id"
+    t.integer "user_id"
+    t.string "content"
   end
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
